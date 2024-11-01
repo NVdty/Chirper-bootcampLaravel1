ceb88788cd74ff3d9a93155229bf2ff68122df3f
NVdty <nadity6@gmail.com>
2024-11-01 16:25:39 +0700

    feature creating chirper

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,20 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+// use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
+    }
     protected $fillable = [
         'name',
         'email',
