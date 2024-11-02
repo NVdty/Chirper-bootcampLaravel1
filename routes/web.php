a866cb2fedec916a48bedb256f5514ff34c43e91
NVdty <nadity6@gmail.com>
2024-11-02 09:18:20 +0700

    delete and notify

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
     });
     
     Route::resource('chirps', ChirpController::class)
-        ->only(['index', 'store', 'edit', 'update'])
+        ->only(['index', 'store', 'edit', 'update', 'destroy'])
         ->middleware(['auth', 'verified']);
  
 require __DIR__.'/auth.php';
